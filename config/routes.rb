9e83463206fdfd0b3aedc76fbcb77f8f00a39d7c
MasanoriIwakura <hagobutler@yahoo.co.jp>
2019-07-30 23:29:31 +0900

    Update style

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :todos
   get 'hello' => 'hello#index'
   get 'hello/:name' => 'hello#show'
+  # get 'hello(/:name) => 'hello#show' とするとパラメータ無しでもエラーにならない
   get 'list/index'
   
 end
